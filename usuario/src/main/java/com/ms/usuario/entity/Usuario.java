3f826a9b4804dbba5ecb143426337c8404024b66
luan.borghesan <35976956+LuanHB@users.noreply.github.com>
2024-08-03 16:47:20 -0300

    Adiciona envio de mensagem para o rabbitmq

--- usuario/src/main/java/com/ms/usuario/entity/Usuario.java
+++ usuario/src/main/java/com/ms/usuario/entity/Usuario.java
@@ -1,6 +1,10 @@
 package com.ms.usuario.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
